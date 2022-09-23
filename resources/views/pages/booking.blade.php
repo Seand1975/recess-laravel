edceae46d133684a3ceeeb8f7fa148edf106a225
seand2003 <mwesigwaduncan@gmal.com>
2022-09-23 16:56:15 +0300

    added quantity to bookings

--- resources/views/pages/booking.blade.php
+++ resources/views/pages/booking.blade.php
@@ -8,25 +8,29 @@
                     <div class="card-header">
                         <div class="row">
                             <div class="col-md-3"><h6>Order Name</h6></div>
-                            <div class="col-md-9"><h6>Description</h6></div>
+                            <div class="col-md-8"><h6>Description</h6></div>
+                            <div class="col-md-1">Quantity</div>
                         </div>
                     </div>
                     <ul class="list-group list-group-flush">
                     <li class="list-group-item"><div class="row">
                         <div class="col-md-3">Shoes</div>
-                        <div class="col-md-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, doloribus facere nemo quasi totam, maxime commodi blanditiis deleniti maiores numquam eos eaque molestiae, perspiciatis recusandae? Asperiores nihil itaque veniam aut.</div>
-                        </div>
+                        <div class="col-md-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, doloribus facere nemo quasi totam, maxime commodi blanditiis deleniti maiores numquam eos eaque molestiae, perspiciatis recusandae? Asperiores nihil itaque veniam aut.</div>
+                        <div class="col-md-1">1</div>
+                    </div>
                     </li>
                     <li class="list-group-item">
                         <div class="row">
                             <div class="col-md-3">Jacket</div>
-                            <div class="col-md-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, doloribus facere nemo quasi totam, maxime commodi blanditiis deleniti maiores numquam eos eaque molestiae, perspiciatis recusandae? Asperiores nihil itaque veniam aut.</div>
+                            <div class="col-md-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, doloribus facere nemo quasi totam, maxime commodi blanditiis deleniti maiores numquam eos eaque molestiae, perspiciatis recusandae? Asperiores nihil itaque veniam aut.</div>
+                            <div class="col-md-1">3</div>
                         </div>
                 </li>
                     <li class="list-group-item">
                         <div class="row">
                             <div class="col-md-3">Watch</div>
-                            <div class="col-md-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, doloribus facere nemo quasi totam, maxime commodi blanditiis deleniti maiores numquam eos eaque molestiae, perspiciatis recusandae? Asperiores nihil itaque veniam aut.</div>
+                            <div class="col-md-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, doloribus facere nemo quasi totam, maxime commodi blanditiis deleniti maiores numquam eos eaque molestiae, perspiciatis recusandae? Asperiores nihil itaque veniam aut.</div>
+                            <div class="col-md-1">2</div>
                         </div>
                     </li>
                     </ul>
