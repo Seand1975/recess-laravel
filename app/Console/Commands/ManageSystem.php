1c1a0da1f0876c7ce1e490741444d92c8dd53fec
DanielHuey <adhueywilan@gmail.com>
2022-09-24 07:42:07 +0300

    fricking fixed all errors

--- app/Console/Commands/ManageSystem.php
+++ app/Console/Commands/ManageSystem.php
@@ -38,25 +38,25 @@ class ManageSystem extends Command
 
       $path = dirname(__DIR__,4)."\\recess-java\\";
       $ankafile = fopen($path."anka.txt","r+");
-      $cronfile = fopen($path."cron.txt","a+");
-      $requestfile = fopen($path."Request.txt","a+");
+      $cronfile = fopen($path."cron.txt","r+");
+      $requestfile = fopen($path."Request.txt","r+");
       while (($line = fgets($ankafile))!==false) {
-
+        sleep(3);
         $work = explode(": ",$line);
 
         $content = explode(",",$work[1]);//content[e.g name,pass,dob & product for register]: 
 
         switch ($work[0]) {
           case 'reg':# name	password	date_of_birth	product	
-            $check = ParticipantPoint::where('name',$content[0])->first();
-            if ($check->name != $content[0]) {
+            $check = Participant::where('name',$content[0])->first();
+            if ($check == null) {
               $participant_->name = $content[0];
               $participant_->password = $content[1];
               $participant_->date_of_birth = $content[2];
               $participant_->product = $content[3];
               $participant_->save();
-              fwrite($cronfile,"session:".$content[0].",".$content[3]);
-              fwrite($cronfile,"success:Registered and Logged in!");
+              fwrite($cronfile,"session:".$content[0].",".$content[3]."\n");
+              fwrite($cronfile,"success:Registered and Logged in!\n");
             }else {
               fwrite($cronfile,"error:".$content[0]." already exists.\n");
             }
@@ -64,8 +64,8 @@ class ManageSystem extends Command
           case 'login':# name	password
             $check = Participant::where('name',$content[0])->where('password',$content[1])->first();
             if ($check->name == $content[0] && $check->password == $content[1]) {
-              fwrite($cronfile,"session:".$content[0].",".$content[3]);
-              fwrite($cronfile,"success:Logged in successfully!");
+              fwrite($cronfile,"session:".$content[0].",".$content[3]."\n");
+              fwrite($cronfile,"success:Logged in successfully!\n");
             }
             break;
           case 'del':# user
@@ -76,16 +76,23 @@ class ManageSystem extends Command
             # sessname name desc price
             # product_name product_description available_quantity rate_per_item posted_by created_at updated_at
             $getter = Participant::where('name',$content[0])->where('product',$content[1])->first();
-            if ($getter->product == $content[1]) {
+            if ($getter != null) {
               $product_->product_name = $content[1];
               $product_->product_description = implode(" ",explode("-",$content[2]));
               $product_->available_quantity = 1;
               $product_->rate_per_item = $content[3];
               $product_->save();
+              fwrite($cronfile,"success:Posted successfully!\n");
             }
             break;
           case 'qty':
             info('Quantity command!!');
+            $getter = Product::where('posted_by',$content[0])->where('product_name',$content[1])->first();
+            if ($getter != null) {
+              $getter->available_quantity = $content[2];
+              $getter->save();
+            }
+            fwrite($cronfile,"success:Updated quantity successfully!\n");
             //update quantity of products
             break;
           
@@ -94,7 +101,12 @@ class ManageSystem extends Command
             break;
         }
       }//*/
+      // unlink($path."anka.txt");
+      // unlink($path."cron.txt");
+      file_put_contents($path."anka.txt","_");
+      file_put_contents($path."cron.txt","_");
       fclose($ankafile);
+      fclose($cronfile);
 
       while (($line = fgets($requestfile))!==false) {
         //name	request_answer	request_time	request_response_time	request_response_seen	
@@ -108,16 +120,19 @@ class ManageSystem extends Command
             $participant[0]->request_response_seen = $details[1];
             break;
           case 'Request':
-            $participant = PerfomanceUpdateRequest::where('name',$details[0])->get();
-            if (count($participant)!=0) {
-              $participant[0]->request_time = $details[1];
-              $participant[0]->save();
+            $participant = PerfomanceUpdateRequest::where('name',$details[0])->first();
+            if ($participant->name == $details[0]) {
+              $participant->request_time = $details[1];
+              $participant->save();
             }else {
               $req_->name = $details[0];
               $req_->request_time = $details[1];
               $req_->request_response_time = $date_;
               $req_->save();
-            }
+              $points = ParticipantPoint::where('name',$details[0])->first();
+              $answer = "response=Name:".$points->name.",Points:".$points->number_of_points."\n";
+              fwrite($requestfile,$answer);
+          }
             break;
           default:
             info("No request.");
