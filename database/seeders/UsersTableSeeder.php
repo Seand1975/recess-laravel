a3c3f8dfb17798d8db4a17312cda4e9634df9884
Brianoop <brianangoda99@gmail.com>
2022-09-24 05:16:31 +0300

    added more users to user table seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,12 +15,127 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Admin Admin',
-            'email' => 'admin@lightbp.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('secret'),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+                [
+                    'id' => 1,
+                    'name' => 'Admin Admin',
+                    'email' => 'admin@lightbp.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('secret'),
+                    'role' => 'ADMIN',
+                    'user_role' => 1,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Brian Angoda',
+                    'email' => 'brianangoda99@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 3,
+                    'name' => 'Payas Mayanja',
+                    'email' => 'mayanja@anka.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 4,
+                    'name' => 'Alex Aturinda',
+                    'email' => 'alexaturinda@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 5,
+                    'name' => 'Michael Wokorach',
+                    'email' => 'michael@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 6,
+                    'name' => 'Melisa Bianca',
+                    'email' => 'melisa@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 7,
+                    'name' => 'Santos Wogongoba',
+                    'email' => 'santos@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 8,
+                    'name' => 'Mary Namuga',
+                    'email' => 'mary@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 9,
+                    'name' => 'Julius Ceaser',
+                    'email' => 'hulius@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 10,
+                    'name' => 'Joel Muhooli',
+                    'email' => 'joel@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'id' => 11,
+                    'name' => 'Bianca',
+                    'email' => 'bianca@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('1234'),
+                    'role' => 'USER',
+                    'user_role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+          ]);
     }
 }
